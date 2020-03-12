e82746774a315c1bbcd2274d717f12e7bc2326ef
Bernard Yan <ybn@me.com>
2020-03-11 20:53:01 -0400

    production env

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,4 @@
 module.exports = {
-    development: {
-        client: 'postgresql',
-        connection: {
-            database: 'crdb',
-            user:     '',
-            password: ''
-        }
-    },
-    production: {
-        client: 'postgresql',
-        connection: process.env.DATABASE_URL
-    }
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
 };
